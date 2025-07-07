ecd028d608214d01f517e359c5758c92b343d1ef
José Moraes <joseaugustomoraes@outlook.com>
2025-07-07 08:32:54 -0300

    Gen 30

--- homes/x86_64-linux/jaugusto/include/fish/functions.nix
+++ homes/x86_64-linux/jaugusto/include/fish/functions.nix
@@ -1,12 +1,4 @@
 {
-  build-nix = {
-    description = "Builds nix home-manager without updating flakes inputs";
-    body = builtins.readFile ./functions/build-nix.fish;
-  };
-  edit-home = {
-    description = "Open home-manager folder in neovim";
-    body = builtins.readFile ./functions/edit-home.fish;
-  };
   preset-picosdk = {
     description = "Creates a folder with the Pico SDK template";
     body = builtins.readFile ./functions/preset-picosdk.fish;
@@ -23,12 +15,8 @@
     description = "Setup for picotool";
     body = builtins.readFile ./functions/setup-picotool.fish;
   };
-  update-pkgs = {
-    description = "Upgrades packages from dnf and flatpak";
-    body = builtins.readFile ./functions/update-pkgs.fish;
-  };
-  update-nix = {
-    description = "Upgrades packages from nix";
-    body = builtins.readFile ./functions/update-nix.fish;
+  update-flake = {
+    description = "Updates system flake inputs";
+    body = builtins.readFile ./functions/update-flake.fish;
   };
 }
