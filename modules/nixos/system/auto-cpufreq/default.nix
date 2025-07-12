5e781f73ea590a865a9792053d2cdc56e1452616
José Moraes <joseaugustomoraes@outlook.com>
2025-07-12 09:29:41 -0300

    12/07/2025

--- modules/nixos/system/auto-cpufreq/default.nix
+++ modules/nixos/system/auto-cpufreq/default.nix
@@ -16,6 +16,18 @@ in
   };
   config = lib.mkIf cfg.enable {
     services.power-profiles-daemon.enable = false;
-    programs.auto-cpufreq.enable = true;
+    programs.auto-cpufreq = {
+      enable = true;
+      settings = {
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+        battery = {
+          governor = "powersave";
+          turbo = "auto";
+        };
+      };
+    };
   };
 }
