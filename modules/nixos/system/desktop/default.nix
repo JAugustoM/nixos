962fb2ed7221c314e5b8fbaaeecf21255c4d6c35
José Moraes <joseaugustomoraes@outlook.com>
2025-07-26 11:53:08 -0300

    26/07/2025

--- modules/nixos/system/desktop/default.nix
+++ modules/nixos/system/desktop/default.nix
@@ -16,7 +16,11 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+
     services.desktopManager.plasma6.enable = true;
   };
 }
